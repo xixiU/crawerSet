fe9bd3a3213add6dacb9d1bc3943a5d941465da8
rongjie.yuan <rongjie.yuan@jiduauto.com>
2024-10-10 14:22:41 +0800

    fx jinkela checkin

--- jinkela签到/main.py
+++ jinkela签到/main.py
@@ -25,24 +25,27 @@ import socket
 #设置所有单例延时
 socket.setdefaulttimeout(singleton_timeout)
 
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
+
 executable_path = '/usr/local/bin/chromedriver'
 service = Service(executable_path)
 def start(name_id:str,password:str):
     # print("chrome_options:%s"%chrome_options._arguments)
     try:
-        options = uc.ChromeOptions()
-
-        # 设置代理
-        options.add_argument(f"--proxy-server={proxy}")
         print("chrome_options:%s"%options._arguments)
 
         driver = uc.Chrome(driver_executable_path= executable_path,options=options)
-        driver = webdriver.Chrome(service = service,options=chrome_options)
+
+        # driver = webdriver.Chrome(service = service,options=chrome_options)
+
         driver.implicitly_wait(singleton_timeout)
         driver.get(main_site)
-        driver.maximize_window()
+        # driver.maximize_window()
 
-        seek_name = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//input[@id='email']")))
+        driver.execute_script(f"window.open('{main_site}', '_blank')")
+        time.sleep(10)
+        driver.switch_to.window(driver.window_handles[1])
+        seek_name = WebDriverWait(driver, 100).until(EC.presence_of_element_located((By.XPATH, "//input[@id='email']")))
         # seek_name = driver.find_element_by_xpath("//input[@placeholder='账号']")
         seek_name.send_keys(name_id)    
 
@@ -55,7 +58,7 @@ def start(name_id:str,password:str):
         seek_but = driver.find_element(By.XPATH,"//button[@id='login']")     
         #并点击搜索 按钮          
         seek_but.click()  
-
+        print('login successful')
         # 找到签到按钮
         seek_btn = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//button[@id='checkin']")))
         seek_btn.click()
@@ -82,7 +85,7 @@ if __name__ == '__main__':
     executor = ThreadPoolExecutor(max_workers=64)
     tasks = []
 
-    # 启动参数
+    # 启动参数 for selenium
     chrome_options = Options()
     prefs = {
         'profile.default_content_setting_values': {
@@ -90,16 +93,34 @@ if __name__ == '__main__':
             # 'javascript': 2  # 禁用js，可能会导致通过js加载的互动数抓取失效
         }
     }
+    chrome_options.add_argument(f'user-agent={user_agent}')
+
     chrome_options.add_experimental_option("prefs", prefs)
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument("window-size=1024,768")
     chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('blink-settings=imagesEnabled=false')
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+
+    # 启动参数 for undetected_chromedriver
+
+    options = uc.ChromeOptions()
+
+    # 设置代理
+    options.add_argument(f"--proxy-server={proxy}")
+    options.add_argument('—-ignore-certificate-errors')
+    options.add_argument("--disable-popup-blocking")
+    options.add_argument("--auto-open-devtools-for-tabs")
+
+    # chrome_options.add_argument('blink-settings=imagesEnabled=false')
     if len(proxy)>0:
         chrome_options.add_argument(f'--proxy-server={proxy}')
     if not debug_mode:
         chrome_options.add_argument('--headless')  # 使用无头谷歌浏览器模式
+        # options.add_argument('--headless')
+    # chrome 升级到129有bug，之后无头模式运行会报错https://github.com/ultrafunkamsterdam/undetected-chromedriver/issues/2030
+
+
 
     for id_pd in id_password_set:
         start(id_pd[0],id_pd[1])
